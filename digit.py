879e76cb18c73ac67dc2994baff572bdba28e16e
tarek na <tboss4322@gmail.com>
2023-12-31 06:01:00 +0100

    new

--- digit.py
+++ digit.py
@@ -3,58 +3,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist
-# ( input -> output )
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-def train_model():
-    global x_train, x_test
+class HandwrittenDigitRecognizer:
+    def __init__(self, model):
+        self.model = tf.keras.models.load_model(model)
 
-    # transform to 1d array
-    x_train = tf.keras.utils.normalize(x_train, axis=1)
-    x_test = tf.keras.utils.normalize(x_test, axis=1)
+    def train_model(dataset, savefile):
+        (x_train, y_train), (x_test, y_test) = dataset.load_data()
 
-    model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(128, activation='relu'))
-    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+        # transform to 1d array
+        x_train = tf.keras.utils.normalize(x_train, axis=1)
+        x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-    model.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
+        model = tf.keras.models.Sequential()
+        model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(128, activation='relu'))
+        model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-    model.fit(x_train, y_train, epochs=3)
+        model.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
 
-    loss, accuracy = model.evaluate(x_test, y_test)
+        model.fit(x_train, y_train, epochs=3)
 
-    print(loss, accuracy)
+        #loss, accuracy = model.evaluate(x_test, y_test)
+        #print(loss, accuracy)
 
-    model.save('handwritten.model')
+        model.save(savefile)
 
-def test_stats():
-    model = tf.keras.models.load_model('handwritten.model')
-
-    loss, accuracy = model.evaluate(x_test, y_test)
-
-    print(loss, accuracy)
-
-def predict(name):
-    model = tf.keras.models.load_model('handwritten.model')
-
-    try:
-        img = cv2.imread(f"./Numbers/{name}.png")[:,:,0]
-        img = np.invert(np.array([img]))
-        prediction = model.predict(img)
-        print(f"Digit is probably : {np.argmax(prediction)}")
-        plt.imshow(img[0], cmap=plt.cm.binary)
-        plt.show()
-        
-    except:
-        print("Error")
+    def predict(self, name):
+        try:
+            img = cv2.imread(name)[:,:,0]
+            img = np.invert(np.array([img]))
+            prediction = self.model.predict(img)
+            print(f"Digit is probably : {np.argmax(prediction)}")
+            plt.imshow(img[0], cmap=plt.cm.binary)
+            plt.show()
+            
+        except:
+            print("Error")
 
 if __name__ == "__main__":
-    #train_model()
-    predict("five")
-    #test_stats()
+    #HandwrittenDigitRecognizer.train_model(tf.keras.datasets.mnist, 'handwritten.model')
+    d = HandwrittenDigitRecognizer('handwritten.model')
+    d.predict("/home/user/Stuff/HandwrittenDigitRecognition/Numbers/four.png")
